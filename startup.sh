5b58e80e05ada8506187a210e495571a10b347cd
AdamYuan <y13916619121@126.com>
2016-06-17 16:12:51 +0800

    Fri Jun 17 16:12:51 CST 2016

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+pkill compton
+compton --backend glx --vsync opengl-swc  &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-xrdb -load .Xresources &
 fcitx &
-pkill wicd-client
-wicd-client -t &
+nm-applet &
 pkill xss-lock
 xss-lock -- slimlock &
 pkill indicator-keylo
@@ -11,3 +11,12 @@ indicator-keylock &
 pkill volumeicon
 volumeicon 
 
+pkill trayer
+source ~/.config/init/vars
+run=""
+while [ -z "$run" ]; do
+	sleep 0.5
+	run=$(ps -e | grep dzen)
+	echo "$run"
+done
+trayer --edge top --align right --widthtype request --height $DZEN_HEIGHT --transparent true --alpha 1 --tint 0x`echo $DZEN_BGCOLOR|tail -c 7` --margin 100 &
